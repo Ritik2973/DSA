d0e894f7875d855d087b835e947ffd01d486f8c1
Ritik2973 <rivaritik73@gmail.com>
2026-08-18 19:16:28 +0530

    Time: 0 ms (100%), Space: 47.6 MB (98.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,40 +1,59 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1;
-        int last = -1;
+
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    public int findFirst(int[] nums, int target) {
 
         int start = 0;
         int end = nums.length - 1;
+        int ans = -1;
 
         while (start <= end) {
+
             int mid = start + (end - start) / 2;
 
             if (nums[mid] == target) {
-                first = mid;
+                ans = mid;
                 end = mid - 1;
-            } else if (nums[mid] < target) {
+            }
+            else if (nums[mid] < target) {
                 start = mid + 1;
-            } else {
+            }
+            else {
                 end = mid - 1;
             }
         }
 
-        start = 0;
-        end = nums.length - 1;
+        return ans;
+    }
+
+    public int findLast(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
 
         while (start <= end) {
+
             int mid = start + (end - start) / 2;
 
             if (nums[mid] == target) {
-                last = mid;
+                ans = mid;
                 start = mid + 1;
-            } else if (nums[mid] < target) {
+            }
+            else if (nums[mid] < target) {
                 start = mid + 1;
-            } else {
+            }
+            else {
                 end = mid - 1;
             }
         }
 
-        return new int[]{first, last};
+        return ans;
     }
 }
