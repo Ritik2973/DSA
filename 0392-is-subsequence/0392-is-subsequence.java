f959290a83648e719d7830ca70b02192587fb514
Ritik2973 <rivaritik73@gmail.com>
2026-09-12 11:14:12 +0530

    Time: 2 ms (73.09%), Space: 42.7 MB (63.17%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,8 +5,9 @@ class Solution {
         while (i < s.length() && j < t.length()) {
             if (s.charAt(i) == t.charAt(j)) {
                 i++;
-            }
-            j++;
+                
+            }j++;
+            
         }return i == s.length();
     }
 }
